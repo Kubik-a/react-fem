650a0837046e2062feb13ea4ee082f9b14d8aa19
Alexandre Kubik <kubik.alexandre@gmail.com>
2023-03-06 18:13:34 +0100

    Adding Carousel using class component and solve issue with Header

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# React v8 course Frontendmasters
+
+---
+
+This project is created following react course by Brian Holt on frontendmasters, goal is to be up to date on React.
+
+## Installation
+
+---
+
+You can try this project
+
+```
+$ git clone https://example.com
+$ cd ../path/to/the/file
+$ npm install
+$ npm run dev
+```
--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import SearchParams from "./SearchParams";
 import Details from "./Details";
@@ -18,7 +18,9 @@ const App = () => {
   return (
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <h1>Adopt Me!</h1>
+        <header>
+          <Link to="/">Adopt Me!</Link>
+        </header>
         <Routes>
           <Route path="/details/:id" element={<Details />} />
           <Route path="/" element={<SearchParams />} />
